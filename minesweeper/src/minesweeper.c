7cc8f79183bb136e38eda4fd976dbac74f878441
nirzadik <nirzzadik@gmail.com>
2025-11-29 15:01:44 +0200

    finished game! v1

--- minesweeper/src/minesweeper.c
+++ minesweeper/src/minesweeper.c
@@ -5,12 +5,22 @@
 
 #define ROWS 5
 #define COLS 5
-#define MINES 10
+#define MINES 2
+
+typedef struct
+{
+    int row;
+    int col;
+    int flag;
+}point;
+
+
 
 
 
 void printBoard(int board[ROWS][COLS]) {
     for (int i = 0; i < ROWS; i++) {
+        printf("|");
         for (int j = 0; j < COLS; j++) {
             if (board[i][j] == -1) {
                 printf(" * |");
@@ -21,6 +31,35 @@ void printBoard(int board[ROWS][COLS]) {
         printf("\n");
     }
 }
+
+void printUserBoard(int userBoard[ROWS][COLS], int board[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++) {
+        printf("|");
+        for (int j = 0; j < COLS; j++) {
+            switch (userBoard[i][j])
+            {
+            case -1: // flagged
+                printf(" F |");
+                break;
+            case 0: // hidden
+                printf(" 0 |");
+                break;
+            case 1: // revealed
+                if (board[i][j] == 0){
+                    printf("   |");
+                }
+                else {
+                    printf(" %d |", board[i][j]);
+                }
+                break;
+            default:
+                break;
+            }
+        }
+        printf("\n");
+    }
+}
+
 
 int compare(const void *a, const void *b) {
     return (*(int*)a - *(int*)b);
@@ -45,21 +84,10 @@ void generateUniqueMines(int mines[]) {
     qsort(mines, MINES, sizeof(int), compare);
 }
 
-
-void initializeBoard(int board[ROWS][COLS]) {
-    int minesCells[MINES];
-    generateUniqueMines(minesCells);
-    int mineCounter = 0;
-    int placeCounter = 0;
+void initializeUserBoard(int board[ROWS][COLS]) {
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
-            if(placeCounter == minesCells[mineCounter]) {
-                board[i][j] = -1;
-                mineCounter++;
-            } else {
-                board[i][j] = 0;
-            }
-            placeCounter++;
+            board[i][j] = 0;
         }
     }
 }
@@ -90,23 +118,139 @@ void SetNeighborCounts(int board[ROWS][COLS]) {
     }
 }
 
+void initializeBoard(int board[ROWS][COLS]) {
+    int minesCells[MINES];
+    generateUniqueMines(minesCells);
+    int mineCounter = 0;
+    int placeCounter = 0;
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            if(placeCounter == minesCells[mineCounter]) {
+                board[i][j] = -1;
+                mineCounter++;
+            } else {
+                board[i][j] = 0;
+            }
+            placeCounter++;
+        }
+    }
+    SetNeighborCounts(board);
+}
 
+point getUserMove() {
+    char line[4];
+    point p;
+    printf("Enter reveal row: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d", &p.row);
+    printf("Enter reveal col: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d", &p.col);
+    while (p.row < 0 || p.row >= ROWS || p.col < 0 || p.col >= COLS) {
+        printf("Invalid input. Please enter values between 0 and %d for rows and 0 and %d for columns.\n", ROWS - 1, COLS - 1);
+        printf("Enter reveal row: ");
+        fgets(line, sizeof(line), stdin);
+        sscanf(line, "%d", &p.row);
+        printf("Enter reveal col: ");
+        fgets(line, sizeof(line), stdin);
+        sscanf(line, "%d", &p.col);
+    }
+    printf("if you want to flag this cell, enter 1, enter 2 to un-flag, else enter 0: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d", &p.flag);
+    return p;
+}
 
+void revealZeros(int userBoard[ROWS][COLS], int board[ROWS][COLS], int row, int col) {
+    if (row < 0 || row >= ROWS || col < 0 || col >= COLS) {
+        return;
+    }
+    if (userBoard[row][col] != 0) {
+        return;
+    }
+    userBoard[row][col] = 1; // Mark as revealed
+    if (board[row][col] == 0) {
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i != 0 || j != 0) {
+                    revealZeros(userBoard, board, row + i, col + j);
+                }
+            }
+        }
+    }
+}
 
+int setUserReveale(int userBoard[ROWS][COLS], int board[ROWS][COLS]) {
+    point p = getUserMove();
+    printf("CHECK: board place: %d\n", board[p.row][p.col]);
+    if (p.flag == 2)
+    {
+        if(userBoard[p.row][p.col] != -1){
+            printf("Cannot un-flag a non-flagged cell. Choose another cell.\n");
+            p = getUserMove();
+        }
+        else{
+            userBoard[p.row][p.col] = 0; // Un-flag the cell
+            return 0;
+        }
+    }
+    while (userBoard[p.row][p.col] != 0)
+    {
+        printf("Cell already revealed or flagged. Choose another cell.\n");
+        p = getUserMove();
+    }
+    if (p.flag == 1)
+    {
+        userBoard[p.row][p.col] = -1; // Mark as flagged
+        return 0;
+    }
+    if (board[p.row][p.col] > 0){
+        userBoard[p.row][p.col] = 1; // Mark as revealed
+    }
+    else if (board[p.row][p.col] == 0){
+        revealZeros(userBoard, board, p.row, p.col);
+    }
+    else if (board[p.row][p.col] == -1){
+        return -1; // Hit a mine
+    }
+    return 0;
+}
 
+int checkWin(int userBoard[ROWS][COLS], int board[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            if (board[i][j] != -1 && userBoard[i][j] != 1) {
+                return 0; // Not all non-mine cells are revealed
+            }
+        }
+    }
+    return 1; // All non-mine cells are revealed
+}
 
 
 
 int main(){
     srand(time(NULL));
     int board[ROWS][COLS];
+    int userBoard[ROWS][COLS];
     initializeBoard(board);
-    printBoard(board);
+    initializeUserBoard(userBoard);
+    printf("welcome to the game!\n");
+    printUserBoard(userBoard, board);
     printf("\n");
-    printf("\n");
-    SetNeighborCounts(board);
-    printBoard(board);
-    // Further implementation goes here
+    do{  
+        if (setUserReveale(userBoard, board) == -1) {
+            goto gameOver;
+        }
+        printUserBoard(userBoard, board);
+        printf("\n");
+    } while (!checkWin(userBoard, board));
+    printf("Congratulations! you won!\n");
+    return 0;
 
+
+    gameOver:
+        printf("Game Over! you lost!\n");
+        printBoard(board);
     return 0;
 }
